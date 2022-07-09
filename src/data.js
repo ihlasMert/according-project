1f81b687a8bd38c6c5e6a9edf1344e85a9dcdf67
İHLASMERT <ihlas1mert@gmail.com>
2022-07-09 18:05:48 +0300

    according with react icons

---
+++ src/data.js
@@ -0,0 +1,21 @@
+
+
+const questions = [
+   { id:1,
+    title:"Do you have any question",
+    info: "Unicorn vinyl poutine brooklyn"
+    },
+   { id:2,
+    title:"Do you have any question",
+    info: "Unicorn vinyl poutine brooklyn"
+    },
+   { id:3,
+    title:"Do you have any question",
+    info: "Unicorn vinyl poutine brooklyn"
+    },
+   { id:4,
+    title:"Do you have any question",
+    info: "Unicorn vinyl poutine brooklyn"
+    }
+]
+export default questions;
